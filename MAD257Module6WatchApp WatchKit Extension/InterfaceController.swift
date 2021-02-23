310936dd7b0555833468b2748d9cae70595183a9
km <35552602+catnap7777@users.noreply.github.com>
2021-02-23 11:15:41 -0600

    initial commit

--- MAD257Module6WatchApp WatchKit Extension/InterfaceController.swift
+++ MAD257Module6WatchApp WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,38 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var flipLabel: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+    }
+    
+    @IBAction func flipButtonPressed() {
+    
+        flipLabel.setText("Flipping the coin.")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.flipLabel.setText("Flipping the coin..")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.flipLabel.setText("Flipping the coin...")
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    let randomFlip = Bool.random()
+                    
+                    if randomFlip { //true
+                        self.flipLabel.setText("It's Heads")
+                    } else {        //false
+                        self.flipLabel.setText("It's Tails")
+                    }
+                }
+            }
+            
+        }
+       
+       
     }
     
     override func willActivate() {
