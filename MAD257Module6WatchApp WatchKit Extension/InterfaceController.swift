fa3a8301153b1429c9347f399c8ac796a2a3df50
km <35552602+catnap7777@users.noreply.github.com>
2021-02-23 10:43:07 -0600

    Initial Commit

---
+++ MAD257Module6WatchApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MAD257Module6WatchApp WatchKit Extension
+//
+//  Created by Karen Mathes on 2/23/21.
+//  Copyright © 2021 TygerMatrix Software. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ MAD257Module6WatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAD257Module6WatchApp
+//
+//  Created by Karen Mathes on 2/23/21.
+//  Copyright © 2021 TygerMatrix Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
